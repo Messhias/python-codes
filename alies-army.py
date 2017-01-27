864c79957311db92629294a1150c86dccebd0766
Fabio William <messhias@gmail.com>
2017-01-27 14:43:23 -0200

    Alien army updates

--- alies-army.py
+++ alies-army.py
@@ -5,6 +5,17 @@ for alien_number in range(30):
 	new_alien = {'color':'green','points':5,'speed':'slow'}
 	aliens.append(new_alien)
 
+for alien in aliens[:3]:
+	if alien['color'] == 'green':
+		alien['color'] = 'yellow'
+		alien['speed'] = 'medium'
+		alien['points'] = 10
+	elif alien['color'] == 'yellow':
+		alien['color'] = 'red'
+		alien['speed'] = 'fast'
+		alien['points'] = 10
+		
+
 #show the 5 firts aliens 
 for alien in aliens[:5]:
 	print(alien)
