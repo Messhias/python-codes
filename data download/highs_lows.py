6756db9670956954b6e414833ce2763f025042f6
William <messhias@gmail.com>
2017-09-13 23:43:54 -0300

    updates

---
+++ data download/highs_lows.py
@@ -0,0 +1,23 @@
+import csv
+from matplotlib import pyplot as plt
+
+filename = 'sitka_weather_07-2014.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    highs = []
+    for row in reader:
+        highs.append(int(row[1]))
+
+fig = plt.figure(dpi=128,figsize=(10,6))
+
+plt.plot(highs,c='red')
+
+plt.title("Daily high temperatures, July 2017",fontsize=24)
+plt.xlabel('',fontsize=16)
+plt.ylabel("Temperature (F)",fontsize=16)
+plt.tick_params(axis='both',which='major',labelsize=16)
+
+plt.show()
